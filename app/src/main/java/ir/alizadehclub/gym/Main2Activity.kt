d24f281cf60836ac85e8e8a18594d5333abbc050
mahdi120012 <mahdi120012@gmail.com>
2020-03-07 17:19:24 +0330

    edit

--- app/src/main/java/ir/alizadehclub/gym/Main2Activity.kt
+++ app/src/main/java/ir/alizadehclub/gym/Main2Activity.kt
@@ -8,5 +8,6 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        hi
     }
 }
